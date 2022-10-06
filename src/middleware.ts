429dbf8c1ae69fe9df43cecc79d2f71bae9c4084
Nur Ahmadi <ahmadinur2202@gmail.com>
2022-10-06 21:39:08 +0800

    update UI buku

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,14 +6,17 @@ import { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
   const session = await getToken({ req, secret: 'super-secret' })
-  console.log(session)
+
   if (!session) {
     if (req.nextUrl.pathname.startsWith('/bimbingan')) {
-
       return NextResponse.redirect(new URL('/login', req.url))
     }
     if (req.nextUrl.pathname.startsWith('/pinjam')) {
       return NextResponse.redirect(new URL('/login', req.url))
+    }
+  } else {
+    if(req.nextUrl.pathname.startsWith('/login')){
+      return NextResponse.redirect(new URL('/', req.url))
     }
   }
   return NextResponse.next()
@@ -21,5 +24,5 @@ export async function middleware(req: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/', '/bimbingan/:path*', '/pinjam/:path*'],
+  matcher: ['/', '/login', '/bimbingan/:path*', '/pinjam/:path*'],
 }
