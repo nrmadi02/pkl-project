2174d4f1a6131e9630d32085f3d8988fb45fe905
Nur Ahmadi <ahmadinur2202@gmail.com>
2022-10-06 22:02:45 +0800

    update UI buku

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,19 +7,23 @@ import { NextRequest } from 'next/server'
 export async function middleware(req: NextRequest) {
   const session = await getToken({ req, secret: 'super-secret' })
 
-  if (!session) {
-    if (req.nextUrl.pathname.startsWith('/bimbingan')) {
+  if (req.nextUrl.pathname.startsWith('/bimbingan')) {
+    if (!session) {
       return NextResponse.redirect(new URL('/login', req.url))
-    }
-    if (req.nextUrl.pathname.startsWith('/pinjam')) {
-      return NextResponse.redirect(new URL('/login', req.url))
-    }
-  } else {
-    if(req.nextUrl.pathname.startsWith('/login')){
-      return NextResponse.redirect(new URL('/', req.url))
     }
   }
-  return NextResponse.next()
+  if (req.nextUrl.pathname.startsWith('/pinjam')) {
+    if (!session) {
+      return NextResponse.redirect(new URL('/login', req.url))
+    }
+    return NextResponse.next()
+  }
+  if (req.nextUrl.pathname.startsWith('/login')) {
+    if (session) {
+      return NextResponse.redirect(new URL('/', req.url))
+    }
+    return NextResponse.next()
+  }
 }
 
 // See "Matching Paths" below to learn more
