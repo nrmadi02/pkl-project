9fd6a7300d519694a07639bc9370336e2fb3a247
Nur Ahmadi <ahmadinur2202@gmail.com>
2022-12-20 18:07:51 +0800

    update stats

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ function defineNextConfig(config) {
 
 export default defineNextConfig({
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   output: "standalone",
   // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
   i18n: {
